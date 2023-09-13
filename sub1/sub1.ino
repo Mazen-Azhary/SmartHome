f38062d40dc9838263b98637cd152760047d310f
Mazen-Azhary <106385298+Mazen-Azhary@users.noreply.github.com>
2023-09-13 22:17:22 +0300

    Update sub1.ino , fixed it a bit but not completed

--- sub1/sub1.ino
+++ sub1/sub1.ino
@@ -1,219 +1,123 @@
 #include <ros.h>
-#include <EEPROM.h>
+#include <std_msgs/Int8.h>
 #include <Servo.h>
-#include <std_msgs/Int32.h>
-
-//define the led and the buzzer
+#include <EEPROM.h>
+// Pin Definitions
 #define RED 10
-#define Buzzer 2
-
-//define the servo
-Servo myServo;
-#define ServoPin 3
-
-//servo2 (bab el sha2a)
-int servosignal=5;
-Servo myServo1 ;
-int pos1 =0 ;
-//define the DC-motor
+#define BUZZER 2
+#define GasSensor A3
+#define echo 5
+#define trig 6
+#define LDR_pin A0
+#define ServoMinimumDistance 50
+#define LDRThreshold 200
+#define DangerousGasThreshold 40
 #define H1 7
 #define H2 8
 #define EN 6
 #define WHITE 5
-////////////////////
-volatile bool isPasswordCorrect = false;
-char storedPassword[] = "123"; 
-//eeprom pass
-
-//initializing variables
-int light = 0;
-
+// Message Symbols to be received from publisher
+#define ServoOnSymbol 1
+#define ServoOffSymbol 2
+#define GasSymbol 3
+#define CurtainsOnSymbol 4
+#define CurtainsOffSymbol 5
 //
+int lightIntensity = 0;
+float dist = 0;
+int servosignal = 5;
+Servo myServo1;
+int pos1 = 0;
 int pos = 0;
-int closed = 0;
-int servo1 = 0;
+
+void SwitchCaseFunction(const std_msgs::Int8& Received_Message) {
+  int Order = Received_Message.data;
+  switch (Order) {
+    case ServoOnSymbol:  
+      servo_on();
+      break;
+    case ServoOffSymbol: 
+      servo_off();
+      break;
+    case GasSymbol: 
+     
+     break;
+    case CurtainsOnSymbol:
+    motoropen();
+    break;
+    case CurtainsOffSymbol:
+    motorclose();
+      break;
+    default:
+      // Handle default case
+      break;
+  }
+}
 
 ros::NodeHandle nh;
+std_msgs::Int8 Received_Message;
+ros::Subscriber<std_msgs::Int8> Arduino2("Orders Channel", &SwitchCaseFunction);
 
-std_msgs::Int32 gas_emergency_msg;
-std_msgs::Int32 servo_msg;
-std_msgs::Int32 light_msg;
-
-
-
-//ISR function
-void interruptFunction() {
-  char receivedPassword[50];
-  for (int i = 0; i < sizeof(storedPassword); i++) {
-    receivedPassword[i] = EEPROM.read(i);
-  }
-  if (strcmp(receivedPassword, storedPassword) == 0) {
-    isPasswordCorrect = true;
-  }
-}
-
-
-
-
-//Servo CallBack function
-void servoCallback(const std_msgs::Int32& msg)
-{
-  servo1 = msg.data;
-
-  if (servo1 < 15)
-  {
-    servo_on();
-    delay(5000);
-    
-  }
-
-  if (servo1 > 15 )
-  {
-    servo_off();
-    delay(5000);
-    
-  }
-}
-
-
-void gasEmergencyCallback(const std_msgs::Int32& msg) {
-  gas_emergency_msg = msg;
-  if (gas_emergency_msg.data > 40) {
-    digitalWrite(RED, HIGH);
-    digitalWrite(Buzzer, HIGH);
-    servo_on();
-    servo2_on();
-    motoropen();
-    
-    delay(10000);
-  }
-  else if (gas_emergency_msg.data > 25) {
-    digitalWrite(RED, LOW);
-    digitalWrite(Buzzer, LOW);
-  } else {
-    digitalWrite(RED, LOW);
-    digitalWrite(Buzzer, LOW);
-  }
-}
-
-
-// LDR CallBack function
-void lightCallback(const std_msgs::Int32& msg)
-{
-  light = msg.data;
-
-  if (light > 200 && closed == 0)
-  {
-    motoropen();
-    delay(5000);
-    motorstop();
-     closed = !closed;
-  }
-
-  if (light < 200 && closed == 1)
-  {
-    motorclose();
-    delay(5000);
-    motorstop();
-    closed = !closed;
-  }
-}
-
-
-
-
-ros::Subscriber<std_msgs::Int32> gas_emergency_sub("gas_sensor_reading", &gasEmergencyCallback);
-ros::Subscriber<std_msgs::Int32> servo_sub("ultrasonic", &servoCallback);
-ros::Subscriber<std_msgs::Int32> LDR_sub("LDR_Topic", &lightCallback);
 
 
 void setup() {
   nh.initNode();
-  
-  myServo.attach(ServoPin);
-  nh.subscribe(servo_sub);
-   myServo.attach(servosignal);
-pinMode(WHITE, OUTPUT);
+  nh.subscribe(Arduino2);
+  myServo1.attach(servosignal);
+  pinMode(WHITE, OUTPUT);
   pinMode(RED, OUTPUT);
-  digitalWrite(RED, LOW);
-  digitalWrite(WHITE, LOW);
-  pinMode(Buzzer, OUTPUT);
-  digitalWrite(Buzzer, LOW);
-  nh.subscribe(gas_emergency_sub);
-
+  pinMode(BUZZER, OUTPUT);
   pinMode(H1, OUTPUT);
   pinMode(H2, OUTPUT);
   pinMode(EN, OUTPUT);
-  nh.subscribe(LDR_sub);
-attachInterrupt(servosignal, interruptFunction, RISING);
-// for some reason here the DigitalPinToInterrupt func was producing compilation errors here so we removed it 
-
+  attachInterrupt(servosignal, interruptFunction, RISING);
 }
 
 void loop() {
-  
-
-  
   nh.spinOnce();
   delay(1000);
 }
 
-
 // Servo On and Off functions
 void servo_on() {
-  for (pos = 0; pos <= 90; pos += 1) {
-    myServo.write(pos);
+  for (pos1 = 0; pos1 <= 90; pos1 += 1) {
+    myServo1.write(pos1);
     delay(5);
   }
-  for (pos = 90; pos >= 0; pos -= 1) {
-    myServo.write(pos);
+  for (pos1 = 90; pos1 >= 0; pos1 -= 1) {
+    myServo1.write(pos1);
     delay(2);
   }
 }
+
 void servo_off() {
-  myServo.write(0);
+  myServo1.write(0);
   delay(2);
 }
 
-
-void servo2_on(){
-     	for(pos1=0 ;pos1<=90 ;pos1+=1) //make servo angle inc
-        {myServo1.write(pos1);
-           delay(5);}
-         for(pos1=90 ;pos1>=0 ;pos1-=1) //make servo angle dec
-            {
-          myServo1.write(pos1);
-                 delay(2);}
+void interruptFunction() {
+  char receivedPassword[50];
+  // Implement the interrupt function
 }
-void servo2_off(){
-  myServo1.write(0);
-  
-        delay(10);  }
 
-// Motor On,Off and Stop Functions
-void motorclose()
-{
-  digitalWrite(WHITE,LOW);
+void motorclose() {
+   digitalWrite(WHITE,LOW);
   analogWrite(EN, 100);
   digitalWrite(H2, HIGH);
   digitalWrite(H1, LOW);
-  delay(1000);
 }
 
-void motoropen()
-{
+void motoropen() {
   digitalWrite(WHITE,HIGH);
   analogWrite(EN, 100);
   digitalWrite(H1, HIGH);
   digitalWrite(H2, LOW);
-  delay(1000);
 }
 
-void motorstop()
-{
-    digitalWrite(WHITE,LOW);
-
+void motorstop() {
+   digitalWrite(WHITE,LOW);
   analogWrite(EN, 0);
   digitalWrite(H1, LOW);
   digitalWrite(H2, LOW);
 }
+
