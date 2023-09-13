2dbacef7b4b99d5a246da58c706923f7a2d19ac3
Mazen-Azhary <106385298+Mazen-Azhary@users.noreply.github.com>
2023-09-13 20:07:24 +0300

    Update pub1.ino using switchcase and symbol approach

--- pub1/pub1.ino
+++ pub1/pub1.ino
@@ -1,83 +1,83 @@
 #include <ros.h>
-#include <std_msgs/Int32.h>
+#include <std_msgs/Int8.h>
 
-//defines for the Buzzer and the gasSensor
+// Pin Definitions
 #define RED 10
-#define BUZZER 2  
+#define BUZZER 2
 #define GasSensor A3
-//////////
 #define echo 5
 #define trig 6
-//defines for LDR 
-#define LDR A0
+#define LDR_pin A0
 
+// Thresholds and Constants
+#define ServoMinimumDistance 50
+#define LDRThreshold 200
+#define DangerousGasThreshold 40
+// Message Symbols to be sent to subscriber switchcase function
+#define ServoOnSymbol 1
+#define ServoOffSymbol 2
+#define GasSymbol 3
+#define CurtainsOnSymbol 4
+#define CurtainsOffSymbol 5
 
-
-//define variables used
 int lightIntensity = 0;
-int measurementTime = 0;
 float dist = 0;
 
 ros::NodeHandle nh;
-std_msgs::Int32 gas_emergency_msg; // this is gas sensor message 
-std_msgs::Int32 LDR_msg; //message containes the reading of the LDR
-std_msgs::Int32 servo_msg; // this is ultrasonic message
+std_msgs::Int8 PublisherMessage;
+ros::Publisher Arduino1("Orders_Channel", &PublisherMessage);//one topic only where symbols will be sent
 
-ros::Publisher ultrasonic("ultrasonic", &servo_msg);
-ros::Publisher gas_emergency_pub("gas_sensor_reading", &gas_emergency_msg);
-ros::Publisher LDR("LDR_Topic", &LDR_msg);
+void setup() {
+  nh.initNode();
+  pinMode(RED, OUTPUT);
+  pinMode(BUZZER, OUTPUT);
+  pinMode(LDR_pin, INPUT);
+  pinMode(trig, OUTPUT);
+  pinMode(echo, INPUT);
+}
 
-void ultrasonic_sensor()
-{
+void loop() {
+  ultrasonicSensor();
+  gasFunction();
+  LDRFunction();
+  nh.spinOnce();
+}
+
+void ultrasonicSensor() {
   digitalWrite(trig, LOW);
   delayMicroseconds(2);
   digitalWrite(trig, HIGH);
   delayMicroseconds(10);
   digitalWrite(trig, LOW);
-  measurementTime = pulseIn(echo, HIGH);
-  dist = measurementTime * 0.034 / 2/10;
-  Serial.print("Distance: ");
-  Serial.print(dist);
-  Serial.println(" cm");
-}
+  unsigned long measurementTime = pulseIn(echo, HIGH);
+  dist = measurementTime * 0.034 / 2;
 
-
-
-void setup() {
-  nh.initNode();
-  pinMode(RED, OUTPUT);
-  digitalWrite(RED, LOW);
-  pinMode(BUZZER, OUTPUT);
-  digitalWrite(BUZZER, LOW);
-  pinMode(LDR, INPUT);
-  pinMode(trig, OUTPUT);
-  pinMode(echo, INPUT);
-
-  nh.advertise(gas_emergency_pub);  //Advertise the publisher
-  nh.advertise(ultrasonic);  //Advertise the ultrasonic
-  nh.advertise(LDR);  //Advertise the LDR
-  
+  while(dist < ServoMinimumDistance) {
+    publishMessage(ServoOnSymbol);//I don't know if this is the best approach but I did a while object is close keep sending servo_on symbol
+  }
+    publishMessage(ServoOffSymbol);
   
 }
 
-void loop() {
-  //initialize the ultrasonic sensor
-  ultrasonic_sensor();
-  //publish the ultrasonic msg
-  servo_msg.data = dist;
-  ultrasonic.publish(&servo_msg);
-
-  //get the gas sensor reading
-  gas_emergency_msg.data = analogRead(GasSensor);  
-  // Publish the gas_emergency_msg
-  gas_emergency_pub.publish(&gas_emergency_msg);
-
-  //get the LDR reading
-  lightIntensity = analogRead(LDR);
-  LDR_msg.data = lightIntensity;
-  //publish the LDR-reading
-  LDR.publish(LDR_msg);
+void gasFunction() {
+  int gasReading = analogRead(GasSensor);
   
-  delay(1000);
-  nh.spinOnce();
+  if (gasReading >= DangerousGasThreshold) {
+    publishMessage(GasSymbol);
+  }
+}
+
+void LDRFunction() {
+  lightIntensity = analogRead(LDR_pin);
+
+  if (lightIntensity > LDRThreshold) {
+    publishMessage(CurtainsOnSymbol);
+  } else {
+    publishMessage(CurtainsOffSymbol);
+  }
+}
+
+void publishMessage(int symbol) {
+  PublisherMessage.data = symbol;
+  Arduino1.publish(&PublisherMessage);
 }
