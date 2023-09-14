a4184b13d477d6279a759aaa289afbfedba42c3f
Mazen-Azhary <106385298+Mazen-Azhary@users.noreply.github.com>
2023-09-14 17:47:36 +0300

    Update sub1.ino

--- sub1/sub1.ino
+++ sub1/sub1.ino
@@ -2,33 +2,31 @@
 #include <std_msgs/Int8.h>
 #include <Servo.h>
 #include <EEPROM.h>
+
 // Pin Definitions
 #define RED 10
 #define BUZZER 2
-#define GasSensor A3
-#define echo 5
-#define trig 6
-#define LDR_pin A0
-#define ServoMinimumDistance 50
-#define LDRThreshold 200
-#define DangerousGasThreshold 40
 #define H1 7
 #define H2 8
 #define EN 6
 #define WHITE 5
-// Message Symbols to be received from publisher
+
+// Message Symbols to be received from the publisher
 #define ServoOnSymbol 1
 #define ServoOffSymbol 2
 #define GasSymbol 3
 #define CurtainsOnSymbol 4
 #define CurtainsOffSymbol 5
-//
-int lightIntensity = 0;
-float dist = 0;
-int servosignal = 5;
+
+int roomservosignal = 5;
+Servo roomservo;
 Servo myServo1;
 int pos1 = 0;
 int pos = 0;
+x
+bool gasEmergencyActive = false; 
+unsigned long gasEmergencyStartTime; // Timestamp when gas emergency started
+const unsigned long gasEmergencyDuration = 10000; // Duration in milliseconds (e.g., 10 seconds)
 
 void SwitchCaseFunction(const std_msgs::Int8& Received_Message) {
   int Order = Received_Message.data;
@@ -40,13 +38,13 @@ void SwitchCaseFunction(const std_msgs::Int8& Received_Message) {
       servo_off();
       break;
     case GasSymbol: 
-     
-     break;
+      GasEmergency();
+      break;
     case CurtainsOnSymbol:
-    motoropen();
-    break;
+      motoropen();
+      break;
     case CurtainsOffSymbol:
-    motorclose();
+      motorclose();
       break;
     default:
       // Handle default case
@@ -58,66 +56,87 @@ ros::NodeHandle nh;
 std_msgs::Int8 Received_Message;
 ros::Subscriber<std_msgs::Int8> Arduino2("Orders Channel", &SwitchCaseFunction);
 
-
-
 void setup() {
   nh.initNode();
   nh.subscribe(Arduino2);
-  myServo1.attach(servosignal);
+  myServo1.attach(roomservosignal);
   pinMode(WHITE, OUTPUT);
   pinMode(RED, OUTPUT);
   pinMode(BUZZER, OUTPUT);
   pinMode(H1, OUTPUT);
   pinMode(H2, OUTPUT);
   pinMode(EN, OUTPUT);
-  attachInterrupt(servosignal, interruptFunction, RISING);
 }
 
 void loop() {
   nh.spinOnce();
+
+  // Check and manage gas emergency state
+  if (gasEmergencyActive) {
+    unsigned long currentTime = millis();
+    if (currentTime - gasEmergencyStartTime >= gasEmergencyDuration) {
+      // Gas emergency duration has elapsed, turn off gas-related actions
+      gasEmergencyActive = false;
+      digitalWrite(BUZZER, LOW);
+      digitalWrite(RED, LOW);
+      motorstop();
+      servo_off();
+    }
+  }
+
   delay(1000);
 }
 
 // Servo On and Off functions
 void servo_on() {
   for (pos1 = 0; pos1 <= 90; pos1 += 1) {
-    myServo1.write(pos1);
+    roomservo.write(pos1);
     delay(5);
   }
+  // Delay to keep the servo on for a specific duration
+  delay(5000); // 5 seconds (adjust as needed)
+  servo_off();
+}
+
+void servo_off() {
   for (pos1 = 90; pos1 >= 0; pos1 -= 1) {
-    myServo1.write(pos1);
+    roomservo.write(pos1);
     delay(2);
   }
 }
 
-void servo_off() {
-  myServo1.write(0);
-  delay(2);
-}
-
-void interruptFunction() {
-  char receivedPassword[50];
-  // Implement the interrupt function
-}
-
 void motorclose() {
-   digitalWrite(WHITE,LOW);
+  digitalWrite(WHITE, LOW);
   analogWrite(EN, 100);
   digitalWrite(H2, HIGH);
   digitalWrite(H1, LOW);
+  // Delay to keep the motor on for a specific duration
+  delay(5000); // 5 seconds (adjust as needed)
+  motorstop();
 }
 
 void motoropen() {
-  digitalWrite(WHITE,HIGH);
+  digitalWrite(WHITE, HIGH);
   analogWrite(EN, 100);
   digitalWrite(H1, HIGH);
   digitalWrite(H2, LOW);
+  // Delay to keep the motor on for a specific duration
+  delay(5000); // 5 seconds (adjust as needed)
+  motorstop();
 }
 
 void motorstop() {
-   digitalWrite(WHITE,LOW);
+  digitalWrite(WHITE, LOW);
   analogWrite(EN, 0);
   digitalWrite(H1, LOW);
   digitalWrite(H2, LOW);
 }
 
+void GasEmergency() {
+  digitalWrite(BUZZER, HIGH);
+  digitalWrite(RED, HIGH);
+  motoropen();
+  servo_on();
+  gasEmergencyActive = true;
+  gasEmergencyStartTime = millis();
+}
